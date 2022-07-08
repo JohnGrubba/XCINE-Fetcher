1868419edfad6e9e0750cd9edcfa4c299c06a3ea
JohnGrubba <nicjontrickshots@gmail.com>
2022-07-08 09:57:23 +0200

    xcine update for films

--- xcine.py
+++ xcine.py
@@ -1,23 +1,29 @@
 import requests, json
 from requests.structures import CaseInsensitiveDict
 
-url = "https://xcine.me/serien-disney-phineas-und-ferb-8450-stream/folge-6"
+url = "https://xcine.me/filme-minions-2015-26may-149-stream/deutsch"
 
 r = requests.get(url)
 
 episode_id = r.text[r.text.find(url, r.text.find('<a class="current"')):]
-episode_id = int(episode_id[:episode_id.find(' title=')].replace('" data-name="" data-episode-id="', '').replace(url, "").replace('"', ''))
-print(episode_id)
+episode_id = episode_id[:episode_id.find(' title=')].replace('" data-name="" data-episode-id="', '').replace(url, "").replace('"', '')
+if episode_id == "":
+    episode_id = r.text[r.text.find('data-episode-id="'):]
+    episode_id = episode_id[:episode_id.find('" title=')]
+    episode_id = episode_id.replace('data-episode-id="', '').replace('"', "")
+episode_id = int(episode_id)
+#print(episode_id)
 mov_id = r.text[r.text.find("var mov_id = "):]
 mov_id = mov_id[:mov_id.find(";")]
-mov_id = int(mov_id.replace("var mov_id = ", "").replace("'", ''))
-print(mov_id)
+mov_id = mov_id.replace("var mov_id = ", "").replace("'", '')
+mov_id = int(mov_id)
+#print(mov_id)
 dta = r.text[r.text.find('$( "#player-holder" ).load( "/movie/load-stream/" + movieData.id + "/" + episode_id + "?" + loadStreamSV, {'):r.text.find("}, function() {")].replace('$( "#player-holder" ).load( "/movie/load-stream/" + movieData.id + "/" + episode_id + "?" + loadStreamSV, {', "").strip()
 dta = dta.replace(":", "=").replace(" ", "")
-print(dta)
+#print(dta)
 
 urlu = f"https://xcine.me/movie/load-stream/{mov_id}/{episode_id}?"
-print(urlu)
+#print(urlu)
 
 headers = CaseInsensitiveDict()
 headers["origin"] = "https://xcine.me"
@@ -30,4 +36,17 @@ resp = requests.post(urlu, headers=headers, data=dta)
 jss = resp.text[resp.text.find("var vip_source = "):]
 jss = jss[:jss.find(";")].replace("var vip_source = ", "")
 jss = json.loads(jss)
-print(jss)
+#print(jss)
+best = jss[0]["file"]
+for format in jss:
+    if format["label"] == "1080p":
+        best = format["file"]
+        break
+if requests.get(best, allow_redirects=False).status_code == 307:
+    print("Successful gathering of Streaming URL")
+    print(best)
+else:
+    print("Failed to gather Streaming URL")
+    print(best)
+    print("Please try again")
+    print("If the problem persists, please contact the developer")
